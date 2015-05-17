26e89066d4a9e831a0c66f0ccddcf731ecbb49f0
TheUltimateHacker <tultimateh@gmail.com>
2015-05-17 10:53:26 -0400

    Initial Commit

---
+++ shiftos_next/Terminal.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Terminal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.txtterm = New System.Windows.Forms.TextBox()
+        Me.tmrfirstuse = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'txtterm
+        '
+        Me.txtterm.BackColor = System.Drawing.Color.Black
+        Me.txtterm.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtterm.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.txtterm.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtterm.Font = New System.Drawing.Font("Courier New", 8.25!)
+        Me.txtterm.ForeColor = System.Drawing.Color.White
+        Me.txtterm.Location = New System.Drawing.Point(0, 0)
+        Me.txtterm.Multiline = True
+        Me.txtterm.Name = "txtterm"
+        Me.txtterm.Size = New System.Drawing.Size(635, 418)
+        Me.txtterm.TabIndex = 0
+        Me.txtterm.Text = "ShiftOS Beta 1.0"
+        '
+        'tmrfirstuse
+        '
+        Me.tmrfirstuse.Interval = 5000
+        '
+        'Terminal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(635, 418)
+        Me.Controls.Add(Me.txtterm)
+        Me.Name = "Terminal"
+        Me.Text = "Terminal"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtterm As System.Windows.Forms.TextBox
+    Friend WithEvents tmrfirstuse As System.Windows.Forms.Timer
+End Class
---
+++ shiftos_next/Terminal.vb
@@ -0,0 +1,146 @@
+﻿Public Class Terminal
+
+    Dim trackpos As Integer = 0
+
+    Public Sub terminal_Innitiate(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.FormBorderStyle = Windows.Forms.FormBorderStyle.None
+        Me.WindowState = FormWindowState.Maximized
+        AddLine("user@shiftos ~$> ")
+        SelectBottom()
+    End Sub
+
+    Public Sub SelectBottom()
+        txtterm.Select(txtterm.Text.Length, 0)
+    End Sub
+
+    Public Sub Interpret(command As String)
+        command = command.ToLower
+        If command Like "help" Then
+            ShowHelp()
+        ElseIf command Like "shutdown" Then
+            Me.Close()
+        ElseIf command Like "" Then
+            'This is here to make it so that the Terminal doesn't say "Wrong Command" if the user doesn't input anything.
+        Else
+            AddLine("Invalid command! Type ""help"" for a list of commands.")
+        End If
+    End Sub
+
+    Public Sub ShowHelp()
+        AddLine("ShiftOS Help" & vbNewLine)
+        AddLine("Usage tips: " & vbNewLine)
+        AddLine(" - The terminal runs in full-screen.")
+        AddLine(" - There are no window managers or desktop environments.")
+        AddLine(" - Applications are fully text-based.")
+        AddLine(" - Terminal commands are case-insensitive.")
+        AddLine(" - The screen can only display Black and White." & vbNewLine)
+        AddLine("Commands: " & vbNewLine)
+        AddLine(" - shutdown: Shuts the system down.")
+        AddLine(" - help: Shows this screen.")
+
+
+    End Sub
+
+    Public Sub AddLine(text As String)
+        txtterm.Text += vbNewLine + text
+        SelectBottom()
+    End Sub
+
+    Dim firstuseconversation As Integer = 0
+
+    Private Sub tmrfirstuse_Tick(sender As Object, e As EventArgs) Handles tmrfirstuse.Tick
+        Select Case firstuseconversation
+            Case 0
+                txtterm.ReadOnly = True
+                AddLine("IP Address 170.245.12.80 is connecting as ""DevX"".")
+            Case 1
+                AddLine("DevX: It seems my updates have completly installed.")
+            Case 2
+                AddLine("DevX: Unfortunately, due to your hard drive being formatted, you have lost all your data.")
+            Case 3
+                AddLine("DevX: However, Don't worry! I've added some pretty cool features to the Terminal to make up for this.")
+            Case 4
+                AddLine("DevX: I can't tell you much, except for ""Type Help for a list of commands"".")
+            Case 5
+                AddLine("DevX: I've got to go, but I'll contact you as I develop more updates; and you test the OS.")
+            Case 6
+                AddLine("User ""DevX"" has disconnected.")
+            Case 7
+                txtterm.ResetText()
+                AddLine("user@shiftos ~$> ")
+                txtterm.ReadOnly = False
+                tmrfirstuse.Stop()
+        End Select
+        firstuseconversation += 1
+    End Sub
+
+    Private Sub txtterm_KeyDown(sender As Object, e As KeyEventArgs) Handles txtterm.KeyDown
+        Select Case e.KeyCode
+            Case Keys.ShiftKey
+                trackpos = trackpos - 1
+            Case Keys.Alt
+                trackpos = trackpos - 1
+            Case Keys.CapsLock
+                trackpos = trackpos - 1
+            Case Keys.ControlKey
+                trackpos = trackpos - 1
+            Case Keys.LWin
+                trackpos = trackpos - 1
+            Case Keys.RWin
+                trackpos = trackpos - 1
+            Case Keys.Right
+                If txtterm.SelectionStart = txtterm.TextLength Then
+                    trackpos = trackpos - 1
+                End If
+            Case Keys.Left
+                If trackpos < 1 Then
+                    e.SuppressKeyPress = True
+                    trackpos = trackpos - 1
+                Else
+                    trackpos = trackpos - 2
+                End If
+            Case Keys.Up
+                e.SuppressKeyPress = True
+                trackpos = trackpos - 1
+            Case Keys.Down
+                e.SuppressKeyPress = True
+                trackpos = trackpos - 1
+        End Select
+
+        If e.KeyCode = Keys.Enter Then
+            e.SuppressKeyPress = True
+            Dim command As String = txtterm.Lines(txtterm.Lines.Length - 1).Replace("user@shiftos ~$> ", "").ToLower
+            Interpret(command)
+
+            If command = "clear" Then
+                txtterm.Text = "user@shiftos ~$> "
+                txtterm.Select(txtterm.Text.Length, 0)
+
+            Else
+                AddLine("user@shiftos ~$> ")
+                txtterm.Select(txtterm.Text.Length, 0)
+            End If
+
+            trackpos = 0
+        Else
+            If e.KeyCode = Keys.Back Then
+            Else
+                trackpos = trackpos + 1
+            End If
+        End If
+
+        If e.KeyCode = Keys.Back Then
+            If trackpos < 1 Then
+                e.SuppressKeyPress = True
+            Else
+                If txtterm.SelectedText.Length < 1 Then
+                    trackpos = trackpos - 1
+                Else
+                    e.SuppressKeyPress = True
+                End If
+            End If
+        End If
+
+        SelectBottom()
+    End Sub
+End Class
